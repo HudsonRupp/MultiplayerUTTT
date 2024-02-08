c7c6d389657dcf8d2cae70fcdc51afa8642706da
Hudson Rupp <hudson.rupp@gmail.com>
2024-02-07 22:30:30 -0600

    Game page

--- www/assets/js/game.js
+++ www/assets/js/game.js
@@ -32,3 +32,67 @@ function updateRoom(info) {
     document.getElementById("capacity").innerHTML = roomInfo.occupants + "/" + roomInfo.capacity
     document.getElementById("status").innerHTML = roomInfo.status
 }
+
+function makeMove(i, j) {
+    //send ws
+
+    changeSquare(i, j, "X")
+
+    console.log(i, j)
+}
+
+function checkSquare() {
+    
+}
+
+function changeSquare(i, j, html) {
+    outI = Math.floor(i / 3)
+    outJ = Math.floor(j / 3)
+    inI = i % 3
+    inJ = j % 3
+    var childNodes = document.getElementById("c" + outI.toString() + outJ.toString()).childNodes
+    innerId = "i" + inI.toString() + inJ.toString()
+    for (var k = 0; k < childNodes.length; k++) {
+        var rowNodes = childNodes[k].childNodes
+        for (var i = 0; i < rowNodes.length; i++) {
+            if (rowNodes[i].id == innerId) {
+                rowNodes[i].innerHTML = html
+            }
+        }
+    }
+}
+
+function makeBoard() {
+    boardDiv = document.getElementById("board")
+    board = "";
+    for (let i = 0; i < 3; i++) {
+      board += "<div class=\"row\">"
+        for (let j = 0; j < 3; j++) {
+            board += `
+                <div class="square" id="c` + i.toString() + j.toString() + `">
+                  ` + getInnerBoard(i, j) + `
+                </div>
+            `
+        }
+      board += "</div>"
+    }
+  
+    boardDiv.innerHTML = board
+}
+
+function getInnerBoard(outI, outJ) {
+  board = "";
+  for (let i = 0; i < 3; i++) {
+      board += "<div class=\"innerRow\">"
+        for (j = 0; j < 3; j++) {
+            board += `
+                <div class="innerSquare" id="i` + i.toString() + j.toString() + `" onclick=makeMove(`+((outI * 3) + i)+`,`+((outJ * 3)+j)+`)>
+                    
+                </div>
+            `
+        }
+      board += "</div>"
+  }
+  return board
+}
+makeBoard()
